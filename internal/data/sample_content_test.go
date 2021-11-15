ee19fbb2fc04d5de2fd359c01deb8a3d4646f23c
Michael Armani <michaelarmani00@gmail.com>
2021-11-15 20:57:31 +0100

    Create sample folder for tests then remove it

--- internal/data/sample_content_test.go
+++ internal/data/sample_content_test.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"os"
+	"path"
 	"testing"
 )
 
@@ -11,14 +12,20 @@ func Test_GetSamplePlaintext(t *testing.T) {
 		t.Fatal("no content found in api")
 	}
 
-	file, err := os.Create("./samples/plaintext.txt")
+	folder := "samples"
+	upsertSampleFolder(folder)
+	file, err := os.Create(path.Join(folder, "plaintext.txt"))
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer file.Close()
 
 	_, err = file.WriteAt(result, 0)
 	if err != nil {
+		t.Fatal(err)
+	}
+
+	file.Close()
+	if err := os.RemoveAll(folder); err != nil {
 		t.Fatal(err)
 	}
 }
