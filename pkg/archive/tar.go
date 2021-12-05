d3f99ec11166ced7bfad54b407d89f66b7d0cc28
Michael Armani <michaelarmani00@gmail.com>
2021-12-05 15:03:40 +0100

    Untar archive

--- pkg/archive/tar.go
+++ pkg/archive/tar.go
@@ -4,6 +4,7 @@ import (
 	"archive/tar"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
@@ -66,6 +67,52 @@ func walk(source string, rootDir string, tarball *tar.Writer) filepath.WalkFunc 
 	}
 }
 
+func untarEntries(tarReader *tar.Reader, target string) error {
+	for {
+		// Read Tar entry
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		// Create corresponding folder
+		path := filepath.Join(target, header.Name)
+		info := header.FileInfo()
+		if info.IsDir() {
+			if err = os.MkdirAll(path, info.Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// If file then untar
+		err = untarEntry(tarReader, path, info)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func untarEntry(tarReader *tar.Reader, path string, info fs.FileInfo) error {
+	perms := os.O_CREATE | os.O_TRUNC | os.O_WRONLY
+	file, err := os.OpenFile(path, perms, info.Mode())
+	if err != nil {
+		return err
+	}
+
+	// Populate with untarred file
+	_, err = io.Copy(file, tarReader)
+	if err != nil {
+		return err
+	}
+	file.Close()
+	return nil
+}
+
 func TarFolder(source string) error {
 	filename := filepath.Base(source)
 	target := fmt.Sprintf("%s.tar", filename)
@@ -78,10 +125,17 @@ func TarFolder(source string) error {
 	defer tarball.Close()
 
 	info, err := os.Stat(source)
-	if err != nil {
-		return nil
-	}
+	archiveFatal(err)
 
 	rootDir := getRootDir(info, source)
 	return filepath.Walk(source, walk(source, rootDir, tarball))
+}
+
+func UntarFolder(tarball, target string) error {
+	reader, err := os.Open(tarball)
+	archiveFatal(err)
+	defer reader.Close()
+	tarReader := tar.NewReader(reader)
+
+	return untarEntries(tarReader, target)
 }
--- pkg/archive/tar_test.go
+++ pkg/archive/tar_test.go
@@ -6,17 +6,44 @@ import (
 	"testing"
 )
 
-func Test_TarFolder(t *testing.T) {
+func createTarTestFile() *os.File {
 	file, err := os.Create("test.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	return file
+}
 
+func tarTestFile(t *testing.T, file *os.File) {
 	if err := TarFolder(file.Name()); err != nil {
 		t.Fatal(err)
 	}
+}
 
+func deleteTestFiles(file *os.File) {
 	file.Close()
 	os.Remove(file.Name())
 	os.Remove(file.Name() + ".tar")
+}
+
+func Test_TarFolder(t *testing.T) {
+	file := createTarTestFile()
+	tarTestFile(t, file)
+	deleteTestFiles(file)
+}
+
+func Test_UnTarFolder(t *testing.T) {
+	file := createTarTestFile()
+	tarTestFile(t, file)
+
+	untarredDir := "untarred"
+	os.Mkdir(untarredDir, os.ModeDir)
+	if err := UntarFolder(file.Name()+".tar", untarredDir); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := os.RemoveAll(untarredDir); err != nil {
+		log.Fatal(err)
+	}
+	deleteTestFiles(file)
 }
