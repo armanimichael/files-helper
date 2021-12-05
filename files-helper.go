797ab70b41216ff0cefbc63c8b951df4508c6ded
Michael Armani <michaelarmani00@gmail.com>
2021-12-05 15:53:04 +0100

    Fix typo

--- files-helper.go
+++ files-helper.go
@@ -20,7 +20,7 @@ func main() {
 	command := flag.String("cmd", "", "Util to run")
 	rootDir := flag.String("root", "./", "Root path")
 	searchPattern := flag.String("pattern", "", "Search pattern")
-	replaceStr := flag.String("replace", "", "What to repalace the search pattern with")
+	replaceStr := flag.String("replace", "", "What to replace the search pattern with")
 	extensionsStr := flag.String("extensions", "", "Lookable file extensions separated by comma (ex. txt,html,go)")
 	backup := flag.Bool("backup", false, "Backup matching file before")
 	flag.Parse()
